b9391d8b80a60fcaee5586f8dafd59854f36f472
Harshwardhan <harshwardhanmane9842@gmail.com>
2025-04-11 14:41:39 +0530

    added hexadecimal calci

--- calculator.py
+++ calculator.py
@@ -20,18 +20,35 @@ def multiply(first_number, second_number):
 def divide(first_number, second_number):
     return first_number / second_number
 
-
+def hex_add(a, b):
+	return hex(int(a, 16) + int(b, 16))
+	
+def hex_sub(a, b):
+	return hex(int(a, 16) - int(b, 16))
+	
+def hex_mul(a, b):
+	return hex(int(a, 16) * int(b, 16))
+	
+def hex_div(a, b):
+	return hex(int(a, 16) / int(b, 16))
+	
 print('Select options.')
 print('1. Add')
 print('2. Subtract')
 print('3. Multiply')
 print('4. Divide')
+print('for hexadecimal number')
+print('1h. Add')
+print('2h. Subtract')
+print('3h. Multiply')
+print('4h. Divide')
+
 
 while True:
     # Take input from the console
-    choice = input('Enter choice(1/2/3/4 or n to cancel): ')
+    choice = input('Enter choice(1/2/3/4/1h/2h/3h/4h or n to cancel): ')
     # Check if choice is one of the five options
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4', '1h', '2h', '3h', '4h'):
         first_number = float(input('Enter first number: '))
         second_number = float(input('Enter second number: '))
 
@@ -46,6 +63,17 @@ while True:
 
         elif choice == '4':
             print(first_number, '/', second_number, '=', divide(first_number, second_number))
+        elif choice == '1h':
+            print(first_number, '+', second_number, '=', hex_add(first_number, second_number))
+
+        elif choice == '2h':
+            print(first_number, '-', second_number, '=', hex_sub(first_number, second_number))
+
+        elif choice == '3h':
+            print(first_number, '*', second_number, '=', hex_mul(first_number, second_number))
+
+        elif choice == '4h':
+            print(first_number, '/', second_number, '=', hex_div(first_number, second_number))
     elif choice == 'n':
         print('Your are successfully logged out!')
         break
